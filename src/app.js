de00ff2992da3c1bf1f507e2d70bce6d701e0683
Talvanes Ferreira de Sousa <talvanes.ferreira.sousa@gmail.com>
2020-06-18 11:03:52 -0300

    Backend Challenge: API to manage GitHub repositories

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 
-// const { uuid } = require("uuidv4");
+const { uuid } = require("uuidv4");
 
 const app = express();
 
@@ -11,23 +11,80 @@ app.use(cors());
 const repositories = [];
 
 app.get("/repositories", (request, response) => {
-  // TODO
+  // list all repos
+  return response.json(repositories);
 });
 
 app.post("/repositories", (request, response) => {
-  // TODO
+  // request body params
+  const {title, url, techs} = request.body;
+
+  // create a new repo
+  const repo = {
+    id: uuid(),
+    title,
+    url,
+    techs,
+    likes: 0
+  }
+
+  repositories.push(repo);
+
+  // 201 Created
+  return response.status(201).json(repo);
 });
 
 app.put("/repositories/:id", (request, response) => {
-  // TODO
+  // only update this resource
+  const {id} = request.params;
+
+  const index = repositories.findIndex(repo => repo.id === id);
+  if (index < 0) {
+    return response.status(400).json({ error: 'Repo not found.' });
+  }
+
+  // update repo
+  const {title, url, techs} = request.body;
+
+  const repo = Object.assign(repositories[index], {title, url, techs});
+  repositories[index] = repo;
+
+  // 200 OK
+  return response.json(repo);
 });
 
 app.delete("/repositories/:id", (request, response) => {
-  // TODO
+  // only delete this resoruce
+  const {id} = request.params;
+
+  const index = repositories.findIndex(repo => repo.id === id);
+  if (index < 0) {
+    return response.status(400).json({ error: 'Repo not found.' });
+  }
+
+  // delete repo
+  repositories.splice(index, 1);
+
+  // 204 No content
+  return response.status(204).send();
 });
 
 app.post("/repositories/:id/like", (request, response) => {
-  // TODO
+  // only "like" this repo
+  const {id} = request.params;
+
+  const index = repositories.findIndex(repo => repo.id === id);
+  if (index < 0) {
+    return response.status(400).json({ error: 'Repo not found.' });
+  }
+
+  // "like" the repo
+  const repo = repositories[index];
+  repo.likes += 1;
+  repositories[index] = repo;
+
+  // 200 OK
+  return response.json(repo);
 });
 
 module.exports = app;
